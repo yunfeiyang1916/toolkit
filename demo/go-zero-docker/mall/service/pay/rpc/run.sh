6dc7945e5dfc03645727df052e920e0f292fdbcc
zhangchanglin <changlin.zhang@splashtop.com>
2022-11-07 21:28:06 +0800

    修改执行脚本

--- demo/go-zero-docker/mall/service/pay/rpc/run.sh
+++ demo/go-zero-docker/mall/service/pay/rpc/run.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 server=pay
-nohup go run ${server}.go -f etc/${server}.yaml >>nohup.out 2>&1 &
+app=${server}_rpc
+printf "start build ${app}\n"
+go build -o ${app}
+if [[ $? != 0 ]]; then
+  printf "build failed\n"
+  exit 101
+fi
+
+printf "start run ${app}\n"
+nohup ./${app} -f etc/${server}.yaml >>nohup.out 2>&1 &
